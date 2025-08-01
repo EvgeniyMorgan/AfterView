ec3e4a04492ce6f9501bc91533141a2059a1f5d7
Моргунов Евгений <104060685+EvgeniyMorgan@users.noreply.github.com>
2025-08-01 17:58:51 +0300

    Add files via upload

---
+++ demo/udemo.pas
@@ -0,0 +1,43 @@
+unit udemo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, AVGauge, AVColorBox,
+  AVBevel;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    AVBevel1: TAVBevel;
+    AVColorBox1: TAVColorBox;
+    AVGauge1: TAVGauge;
+    AVGauge10: TAVGauge;
+    AVGauge11: TAVGauge;
+    AVGauge2: TAVGauge;
+    AVGauge3: TAVGauge;
+    AVGauge4: TAVGauge;
+    AVGauge5: TAVGauge;
+    AVGauge6: TAVGauge;
+    AVGauge7: TAVGauge;
+    AVGauge8: TAVGauge;
+    AVGauge9: TAVGauge;
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
